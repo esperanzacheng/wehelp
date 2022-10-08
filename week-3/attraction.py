2122936c706fecc1b384ba876b8c848d34fb0a56
esperanzacheng <60840204+esperanzacheng@users.noreply.github.com>
2022-10-08 14:42:20 +0800

    Add files via upload

---
+++ week-3/attraction.py
@@ -0,0 +1,21 @@
+# import urllib
+import urllib.request as request
+import json
+import csv
+import pandas as pd
+
+# store the link as src
+src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
+with request.urlopen(src) as response:
+    data = pd.read_json(response)
+
+alist = data["result"]["results"]
+
+with open("data.csv", "w", encoding="utf-8", newline="") as file:
+    for attraction in alist:
+        if int(attraction["xpostDate"][0:4]) >= 2015:
+            file.write(attraction["stitle"]+",")
+            file.write(attraction["address"][5:8]+",")
+            file.write(attraction["longitude"]+",")
+            file.write(attraction["latitude"]+",")
+            file.write(attraction["file"].lower().split("jpg")[0]+"jpg"+"\n")
